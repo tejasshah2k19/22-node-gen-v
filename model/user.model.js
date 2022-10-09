1477b5941121a0d0a7e4d1ec6ce434f924a7fdbd
tejas <tejasshah@gmail.com>
2022-10-09 14:49:48 +0530

    node mongoose

---
+++ model/user.model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+
+const UserSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    email: String,
+    password: String
+});
+
+
+
+module.exports  =  mongoose.model("User",UserSchema)
+//users 
+ 
